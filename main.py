07cf464c85b6f9c0632b781d3d6f78a158228b28
Anton Travkin <travkin.rh@gmail.com>
2025-03-22 17:58:19 +0300

    megafon

--- main.py
+++ main.py
@@ -12,22 +12,20 @@ locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
 parent_dir = Path("w")  
 workbook = openpyxl.load_workbook("w/Заявки ВОЛС 2022 - 2025.xlsx")
 sheet = workbook["2025"] 
-
 # Проходим по всем строкам и столбцам
-   
 for row in sheet.iter_rows(min_row=45, max_row=56, min_col=1, max_col=sheet.max_column):  
     number = row[0].value  
     date = row[3].value.strftime("%d.%m.%Y")
     time = row[3].value.strftime("%H:%M")
     date_folder = row[3].value.strftime("%m_%Y")
     date_full = row[3].value.strftime("%d %B %Y")
-    folder_name = (str(row[2].value)+' '+str(date)+' '+str(row[5].value))  
+    folder_name = (str(row[2].value)+' '+str(date)+' '+str(row[4].value)+' '+str(row[6].value)+' '+str(row[5].value))  
     operator =  row[1].value
-    photo = 'фото'
     child_dir = parent_dir / operator / "АВР_ДР" / "на оформлении" /  date_folder/ folder_name 
     child_dir.mkdir(parents=True, exist_ok=True) 
-    child_photo = parent_dir / operator / "АВР_ДР" / "на оформлении" / date_folder / folder_name / photo
+    child_photo = parent_dir / operator / "АВР_ДР" / "на оформлении" / date_folder / folder_name / "Фото"
     child_photo.mkdir(parents=True, exist_ok=True) 
+    
     """Megafon"""
     if (operator == 'Мегафон'):
         # Исходный файл и путь назначения
@@ -57,16 +55,16 @@ for row in sheet.iter_rows(min_row=45, max_row=56, min_col=1, max_col=sheet.max_
             exit()       
         # Вносим изменения 
         ws["C10"] = str(row[2].value) + " от " + date
-        ws_akt["B2"] = " от " + date_full
-        ws_akt["B7"] = f"1. {date} г. Исполнитель оказал, а Заказчик принял работы по договору № РТК-ВОЛС-24-26 от 15.03.2024 г., а именно:"
-        ws_dr["E4"] = ws_dr["D15"] = ws_dr["D17"] = date_full
-        ws_dr["E15"] = time       
         workbook_tg = openpyxl.load_workbook("w/list.xlsx")   
         tg = workbook_tg["Лист1"]       
         for tg in tg.iter_rows(min_row=1, max_row=tg.max_row, min_col=1, max_col=tg.max_column):     
             if (str(tg[0].value) == str(number)):
                 ws["D45"] = tg[11].value * 2
-        workbook_tg.close()       
+        workbook_tg.close()  
+        ws_akt["B2"] = " от " + date_full
+        ws_akt["B7"] = f"1. {date} г. Исполнитель оказал, а Заказчик принял работы по договору № РТК-ВОЛС-24-26 от 15.03.2024 г., а именно:"
+        ws_dr["E4"] = ws_dr["D15"] = ws_dr["D17"] = date_full
+        ws_dr["E15"] = time       
         # Сохраняем файл
         wb.save(destination_file)
         wb.close()
