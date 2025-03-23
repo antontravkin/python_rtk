ca66038fc6b47fbc9e09ad57885fbf5ddb5ff9c6
Anton Travkin <travkin.rh@gmail.com>
2025-03-23 22:24:55 +0300

    телеграм работает. таймер не проверял

--- telegram_rtk.py
+++ telegram_rtk.py
@@ -1,29 +1,27 @@
 import time
 import schedule
 from telegram import Bot
+import asyncio
 
-print(telegram.__version__)
 
 # Токен бота и ID чата
 TOKEN = "7473927237:AAHRrI0-f8kPHuS5MhNeg9tkh5y9DecnTUQ"  # Замени на свой токен
 CHAT_ID = "-4614478695"  # Замени на ID чата
 
 bot = Bot(TOKEN)
+bot.send_message(chat_id=CHAT_ID, text="Привет! 🚀 Это тестовое сообщение!")
+print("✅ Сообщение отправлено!")
 
 
-def send_message():
-    try:
-        bot.send_message(
-            chat_id=CHAT_ID, text="Привет! Это автоматическое сообщение 🚀")
-        print("✅ Сообщение отправлено!")
-    except Exception as e:
-        print(f"❌ Ошибка отправки сообщения: {e}")
+def job():
+    asyncio.run(send_message())
 
 
-# Отправка каждые 5 минут
-schedule.every(5).minutes.do(send_message)
+# Запланировать отправку в 9:00 утра
+schedule.every().day.at("09:00").do(job)
+
+print("Бот запущен. Ожидание времени отправки...")
 
 while True:
     schedule.run_pending()
-    print("🔄 Ожидание...")
-    time.sleep(60)
+    time.sleep(60)  # Проверяет расписание каждую минуту
