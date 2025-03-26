7c2fa499b9343fe565da59defce0c6c70d7c7998
Anton Travkin <travkin.rh@gmail.com>
2025-03-26 07:06:34 +0300

    test

--- part 2/Lesson 1/Конспект_25.03.2025.py
+++ part 2/Lesson 1/Конспект_25.03.2025.py
@@ -1,3 +1,15 @@
 """Конспект 25.03.2025"""
 
 print('hello')
+
+n = int(input('Введите число: '))
+steps = 0
+
+while n != 1:
+    if n % 2 == 0:
+        n //= 2
+    else:
+        n = 3 * n + 1
+    steps += 1
+
+print(steps)
